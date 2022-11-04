e899d64083c764b8a66b2a803a1697d18ea5c4a3
MaximoMeza3150 <75499931+MaximoMeza3150@users.noreply.github.com>
2022-11-04 18:03:33 -0500

    5to commit

--- index.py
+++ index.py
@@ -1,8 +1,9 @@
 from app import app
 from utils.db import db
+import os
 
 with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0:$PORT')
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
