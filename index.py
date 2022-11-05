141621d886ce67e7fa096923801aa44183af5293
MaximoMeza3150 <75499931+MaximoMeza3150@users.noreply.github.com>
2022-11-04 21:37:34 -0500

    7mo commit

--- index.py
+++ index.py
@@ -6,4 +6,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("$PORT", default=5000))
+    app.run(host='0.0.0.0', port=os.getenv('PORT'), debug=True)
+    # app.run(port=os.getenv('PORT', default=5000), debug=True,)
